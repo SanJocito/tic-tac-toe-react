5a74b0e46794d8e50a93a66b0fc4a375c3027761
johan <johanna.pulli@outlook.com>
2023-01-02 11:04:59 +0200

    final

--- tic-tac-toe-react/src/index.js
+++ tic-tac-toe-react/src/index.js
@@ -1,29 +1,60 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 
 const Square = (props) => {
   return (
-    <div className="square">
+    <button 
+    className="square"
+    onClick={props.onClickEvent}
+    >
     {props.value}
-    </div>
+    </button>
   )
 }
 const Board = () => {
+  const initialSquares = Array(9).fill(null);
+  const[squares, setSquares] = useState(initialSquares);
+  const[XisNext, setXisNext] = useState(true);
+
+  //drawing values on board when clicked
+  const handleClickEvent = (i) => {
+   const newSquares = [...squares];
+
+   //Winner
+   //if there is a winner, no more X or O can be drawn on the board even if clicked
+   const winnerDeclared = Boolean(calculateWinner(newSquares));
+   const squareFilled = Boolean(newSquares[i]);
+   if(winnerDeclared || squareFilled){
+    return;
+   }
+
+   newSquares[i] = XisNext ? 'X' : 'O'; //ternary operator -> condition ? expression1 : expression2
+   setSquares(newSquares);
+   setXisNext(!XisNext); //boolean check and taking turns
+  }
+
   const renderSquare = (i) => { 
     return (
-      <Square value={i} />
+      <Square value={squares[i]} 
+      onClickEvent={() => handleClickEvent(i)}
+      />
     );
   };
-
+  const winner = calculateWinner(squares);
+  const status = winner ?
+  `Winner is ${winner}` :
+  `Next player: ${XisNext ? 'X' : 'O'}`;
+  
   return (
     <div style={{
       backgroundColor: '#D3CBC7',
-      margin: 10,
-      padding: 20
+      margin: 20,
+      padding: 10,
+      color: 'black'
     }}>
-      BOARD
+      <div>{status}</div>
       <div className='boardRow'>
       {renderSquare(0)}{renderSquare(1)}{renderSquare(2)}
       </div>
@@ -37,13 +68,33 @@ const Board = () => {
   );
 };
 
+const Instructions = () =>{
+  return(
+    <div style={{
+      margin: 80
+    }}>
+      Tic-Tac-Toe
+      <ol>
+        <li>Player X starts the game</li>
+        <li>Three of the same values vertically, horizontally or diagonally wins the game</li>
+        <li>Refresh the page to play again</li>
+        <li>Have fun!</li>
+      </ol>     
+    </div>
+  )
+}
+
 
 const GameTicTacToe = () =>{
-  return (
-    <div classname="game">
-      Tic-Tac-Toe
-      <Board/>
-    </div>
+  return (  
+   <div style={{
+    alignItems: 'center',
+    justifyContent: 'center',
+    display: 'flex'
+   }}>    
+    <Instructions/>
+    <Board/>
+   </div>
   );
 };
 
@@ -52,4 +103,18 @@ ReactDOM.render(
   document.getElementById('root')
 )
   
+function calculateWinner(squares){
+  const lines = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], //horizontal (rows)
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], //vertical (columns)
+    [0, 4, 8], [2, 4, 6] //diagonal
+  ];
 
+  for (let line of lines){
+    const [a, b, c] = line;
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+      return squares[a]; //X or O
+    }
+  }
+  return null;
+}
